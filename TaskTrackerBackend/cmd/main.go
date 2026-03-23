165c213f8541da234c8747a30ba159872c6f8123
Elus <Elus>
2026-03-23 22:16:41 +0700

    bug fix

--- TaskTrackerBackend/cmd/main.go
+++ TaskTrackerBackend/cmd/main.go
@@ -18,7 +18,7 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, ngrok-skip-browser-warning")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
--- TaskTrackerFrontend/ngrokinstruction.md
+++
@@ -1,4 +0,0 @@
-npm install -g ngrok
-ngrok --version
-ngrok config add-authtoken 3BFuXMzu9OdJ59simSKwk5XBuJ7_2PwBrEJGA9JECHZScuwC6
-ngrok http 5173
