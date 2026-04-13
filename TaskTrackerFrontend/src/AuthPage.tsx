3bbaa1ec48205082d54ebce3d579cf719eb9cee0
Elus1v3 <tdvjfgvtcxdycchf@gmail.com>
2026-04-13 14:16:58 +0700

    registration: send role=developer as temp workaround for user_role_check

--- TaskTrackerFrontend/src/AuthPage.tsx
+++ TaskTrackerFrontend/src/AuthPage.tsx
@@ -26,7 +26,7 @@ export default function AuthPage() {
       const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify(isLogin ? { email, password } : { email, password, role: 'developer' }),
       });
 
       const data = await response.json();
